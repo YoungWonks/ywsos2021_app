20fb03701fe21ef5b1fba5bc724830b5863cd048
Saketh Parimi <sakethparimi@Sakeths-MacBook-Pro.local>
2021-07-03 09:34:14 -0700

    added gradient background to the scaffold

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,96 @@
+import 'package:hexcolor/hexcolor.dart';
+import 'package:ywsos2021_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ywsos2021_app/screens/login_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+// const HomeScreen({Key? key}) : super(key: key);
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  var click = 0;
+
+  void goLogOn() async {
+    print("home");
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+
+    setState(() {});
+    click = 0;
+  }
+
+  void goHome() async {
+    print("home");
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+
+    setState(() {});
+    click = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (context) => LoginScreen(),
-                ),
-              );
-            },
-            icon: Icon(Icons.logout),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.blueAccent,
+          child: ListView(
+            children: [
+              ListTile(
+                title: Text("Options"),
+              ),
+              ListTile(
+                title: Text("Home Screen"),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  print("homescreen");
+                  //If Click Variable =0, Call function for home screen, set click to 1
+                  if (click == 0) goHome();
+                  click = 1;
+                },
+              ),
+              ListTile(
+                title: Text("Add a Complaint"),
+                leading: Icon(Icons.error),
+                onTap: () {
+                  print("Complaint");
+                  //If Click Variable =0, Call function for home screen, set click to 1
+                },
+              ),
+              ListTile(
+                title: Text("Login"),
+                leading: Icon(Icons.login),
+                onTap: () {
+                  print("Login");
+                  if (click == 0) goLogOn();
+                  click = 1;
+                  //If Click Variable =0, Call function for home screen, set click to 1
+                },
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('App'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF97AC94),
+              Color(0xFF5C745C),
+              Color(0xFFA2C08B),
+              Color(0xFF64919F)
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,86 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:ywsos2021_app/screens/home_screen.dart';
-// import 'package:ywsos2021_app/widgets/build_input_decoration.dart';
-
-// class LoginScreen extends StatefulWidget {
-//   LoginScreen({Key? key}) : super(key: key);
-
-//   @override
-//   _LoginScreenState createState() => _LoginScreenState();
-// }
-
-// class _LoginScreenState extends State<LoginScreen> {
-//   bool isLogin = true;
-//   TextEditingController _emailTextController = TextEditingController();
-
-//   TextEditingController _passwordTextController = TextEditingController();
-
-//   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-//   @override
-//   void dispose() {
-//     _emailTextController.dispose();
-//     _passwordTextController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Login'),
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: TextFormField(
-//               controller: _emailTextController,
-//               decoration: buildInputDecoration(
-//                   label: 'Email', hintText: 'john@gmail.com'),
-//             ),
-//           ),
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: TextFormField(
-//               controller: _passwordTextController,
-//               decoration: buildInputDecoration(label: 'Password', hintText: ''),
-//             ),
-//           ),
-//           TextButton(
-//             style: TextButton.styleFrom(
-//               primary: Colors.white,
-//               padding: EdgeInsets.all(15),
-//               shape: RoundedRectangleBorder(
-//                 borderRadius: BorderRadius.circular(4),
-//               ),
-//               backgroundColor: Colors.amber,
-//               textStyle: TextStyle(
-//                 fontSize: 18,
-//               ),
-//             ),
-//             onPressed: () {
-//               if (_formKey.currentState!.validate()) {
-//                 Navigator.of(context).pushReplacement(
-//                   MaterialPageRoute(
-//                     builder: (_) => HomeScreen(),
-//                   ),
-//                 );
-//               }
-//             },
-//             child: Text('Sign In'),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:ywsos2021_app/widgets/create_account_form.dart';
 import 'package:ywsos2021_app/widgets/login_form.dart';
+import 'package:ywsos2021_app/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -97,75 +18,139 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailTextController = TextEditingController();
   final TextEditingController _passwordTextController = TextEditingController();
   @override
+  var click = 0;
+
+  void goLogOn() async {
+    print("home");
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+
+    setState(() {});
+    click = 0;
+  }
+
+  void goHome() async {
+    print("home");
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomeScreen()));
+
+    setState(() {});
+    click = 0;
+  }
+
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.teal,
-              ),
+      child: Scaffold(
+        drawer: Drawer(
+          child: Container(
+            color: Colors.blueAccent,
+            child: ListView(
+              children: [
+                ListTile(
+                  title: Text("Options"),
+                ),
+                ListTile(
+                  title: Text("Home Screen"),
+                  leading: Icon(Icons.home),
+                  onTap: () {
+                    //If Click Variable =0, Call function for home screen, set click to 1
+                    if (click == 0) goHome();
+                    click = 1;
+                  },
+                ),
+                ListTile(
+                  title: Text("Add a Complaint"),
+                  leading: Icon(Icons.error),
+                  onTap: () {
+                    print("Complaint");
+                    //If Click Variable =0, Call function for home screen, set click to 1
+                  },
+                ),
+                ListTile(
+                  title: Text("Login"),
+                  leading: Icon(Icons.login),
+                  onTap: () {
+                    print("Login");
+                    if (click == 0) goLogOn();
+                    click = 1;
+                    //If Click Variable =0, Call function for home screen, set click to 1
+                  },
+                ),
+              ],
             ),
-            Column(children: [
-              Text(
-                isCreateAccountClicked != true ? 'Sign In' : 'Sign Up',
-                style: Theme.of(context).textTheme.headline5,
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.teal,
+                ),
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Column(
-                children: [
-                  SizedBox(
-                    width: 300,
-                    height: 300,
-                    child: isCreateAccountClicked != true
-                        ? LoginForm(
-                            formKey: _formKey,
-                            emailTextController: _emailTextController,
-                            passwordTextController: _passwordTextController,
-                          )
-                        : CreateAccountForm(
-                            formKey: _formKey,
-                            emailTextController: _emailTextController,
-                            passwordTextController: _passwordTextController,
-                          ),
-                  ),
-                  TextButton.icon(
-                    icon: Icon(Icons.portrait_rounded),
-                    style: TextButton.styleFrom(
-                      primary: Colors.red,
-                      textStyle: TextStyle(
-                        fontSize: 18,
-                        fontStyle: FontStyle.italic,
+              Column(children: [
+                Text(
+                  isCreateAccountClicked != true ? 'Sign In' : 'Sign Up',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: 300,
+                      height: 300,
+                      child: isCreateAccountClicked != true
+                          ? LoginForm(
+                              formKey: _formKey,
+                              emailTextController: _emailTextController,
+                              passwordTextController: _passwordTextController,
+                            )
+                          : CreateAccountForm(
+                              formKey: _formKey,
+                              emailTextController: _emailTextController,
+                              passwordTextController: _passwordTextController,
+                            ),
+                    ),
+                    TextButton.icon(
+                      icon: Icon(Icons.portrait_rounded),
+                      style: TextButton.styleFrom(
+                        primary: Colors.red,
+                        textStyle: TextStyle(
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          if (!isCreateAccountClicked) {
+                            isCreateAccountClicked = true;
+                          } else
+                            isCreateAccountClicked = false;
+                        });
+                      },
+                      label: Text(
+                        isCreateAccountClicked
+                            ? 'Already have an account?'
+                            : 'Create Account',
                       ),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        if (!isCreateAccountClicked) {
-                          isCreateAccountClicked = true;
-                        } else
-                          isCreateAccountClicked = false;
-                      });
-                    },
-                    label: Text(
-                      isCreateAccountClicked
-                          ? 'Already have an account?'
-                          : 'Create Account',
-                    ),
-                  ),
-                ],
-              )
-            ]),
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.teal,
+                  ],
+                )
+              ]),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.teal,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
