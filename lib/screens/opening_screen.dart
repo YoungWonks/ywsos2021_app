9e33a2b63dd8988de1bd7a99546879a1a716aab8
Saketh Parimi <sakethparimi@Sakeths-MacBook-Pro.local>
2021-07-04 12:06:57 -0700

    finished opening screen

--- lib/screens/opening_screen.dart
+++ lib/screens/opening_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ywsos2021_app/screens/login_screen.dart';
+import 'package:ywsos2021_app/screens/register_screen.dart';
 
 class OpeningScreen extends StatelessWidget {
   const OpeningScreen({Key? key}) : super(key: key);
@@ -41,6 +43,89 @@ class OpeningScreen extends StatelessWidget {
               ),
             ],
           ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 300,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0.0, 1.0), //(x,y)
+                    blurRadius: 8.0,
+                  ),
+                ],
+                shape: BoxShape.circle,
+                color: Color(0xFF97AC94),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  './assets/images/holding_phone.png',
+                  height: 287.73,
+                  width: 164.69,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Click, Calculate \n and Repair',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (_) => RegisterScreen(),
+                      ),
+                    );
+                  },
+                  child: Text("Sign Up"),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      primary: Color(0xFF97AC94), // background
+                      onPrimary: Colors.white,
+                      padding: EdgeInsets.only(
+                          left: 41,
+                          right: 41,
+                          top: 14,
+                          bottom: 20) // foreground
+                      ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (_) => LoginScreen(),
+                      ),
+                    );
+                  },
+                  child: Text("Sign In"),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      primary: Color(0xFF97AC94), // background
+                      onPrimary: Colors.white,
+                      padding: EdgeInsets.only(
+                          left: 41,
+                          right: 41,
+                          top: 14,
+                          bottom: 20) // foreground
+                      ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:ywsos2021_app/widgets/create_account_form.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
